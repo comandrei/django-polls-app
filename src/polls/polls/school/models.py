afb73b4d58005702654827563b340cfcc9143de2
Andrei Coman <comandrei@gmail.com>
2020-05-20 20:18:43 +0300

    Course changes

--- src/polls/polls/school/models.py
+++ src/polls/polls/school/models.py
@@ -2,16 +2,42 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-from django.db.transaction import atomic
 # Create your models here.
 
-class Courses(models.Model):
+class TimeTrackerModel(models.Model):
+    class Meta:
+        abstract = True
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+class CourseManager(models.Manager):
+
+    def seniors(self):
+        return self.filter(an=4)
+
+    def juniors(self):
+        return self.filter(an__in=[1, 2])
+
+class Courses(TimeTrackerModel):
+
+    class Meta:
+        ordering = ("an", "-title")
+        get_latest_by = ("modified_at", )
+        unique_together = ("title", "an")
+
     title = models.CharField(max_length=100, unique=True)
+    an = models.IntegerField()
+
+    objects = CourseManager()
 
     def __str__(self):
-        return self.title
+        return self.title + ' ' + str(self.an) 
 
-class Student(models.Model):
+
+# Courses.objects.filter(an=4)
+# Courses.objects.seniors()
+
+class Student(TimeTrackerModel):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     an = models.IntegerField()
     courses = models.ManyToManyField(Courses)
@@ -19,3 +45,12 @@ class Student(models.Model):
 
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name
+
+
+# for course in Courses.objects.all():
+#     course.an = course.an + 1
+#     course.save() # update courses set an = x where id = x.id
+
+
+# update courses set an = an + 1 
+
