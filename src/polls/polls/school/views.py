cf57d44fa299b3e6ed0abfa2e56e2dcb5b0a47d5
Andrei Coman <comandrei@gmail.com>
2020-05-25 19:49:41 +0300

    Displa content

--- src/polls/polls/school/views.py
+++ src/polls/polls/school/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import Courses
 # Create your views here.
+
+def index(request):
+    courses = Courses.objects.all()
+    context = {
+        'nume': 'Test123123',
+        'courses': courses,
+        'title': 'ana are mere'
+    }
+    return render(request, 'index.html', context)
+
