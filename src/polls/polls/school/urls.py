4a28ae43b1c92b23a013cadcb34ca4fb20489d31
Andrei Coman <comandrei@gmail.com>
2020-05-26 09:21:32 +0300

    Carnet de note

--- src/polls/polls/school/urls.py
+++ src/polls/polls/school/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, list_student, carnet_note
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('students/<int:cucu_student_id>', carnet_note, name="carnet-note"),
+    path('students', list_student)
 ]
--- src/polls/polls/school/views.py
+++ src/polls/polls/school/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Courses
+from .models import Courses, Student
 # Create your views here.
 
 def index(request):
     courses = Courses.objects.all()
+    request.session['views'] = request.session.get('views', 0) + 1
     context = {
         'nume': 'Test123123',
         'courses': courses,
         'title': 'ana are mere'
     }
     return render(request, 'index.html', context)
+
+def list_student(request):
+    students = Student.objects.all()
+    context = {
+        'students': students
+    }
+    return render(request, 'students.html', context)
+
+def carnet_note(request, cucu_student_id):
+    student = get_object_or_404(Student, pk=cucu_student_id)
+    context= {
+        "student": student,
+    }
+    return render(request, 'carnet_note.html', context)
 
