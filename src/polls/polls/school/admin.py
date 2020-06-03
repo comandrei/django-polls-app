27f0a3ddf8f7f0c97b0f476dfb2cc7ac4e21e78d
Andrei Coman <comandrei@gmail.com>
2020-06-03 19:51:57 +0300

    Show admin integration

--- src/polls/polls/school/admin.py
+++ src/polls/polls/school/admin.py
@@ -3,7 +3,32 @@ from django.contrib import admin
 from .models import Student, Courses, Client, Reprezentant
 # Register your models here.
 
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'an')
+    search_fields = ('title', 'username', 'email')
+    fieldsets = [
+                (None, {
+										'fields': ['title', ],
+										#'classes': ('collapse', )
+                }), 
+                ('Advanced Options', {
+										'fields': ['an'],
+										'classes': ('collapse', )
+                })
+                ]
+    hidden_fields = ('an', )
+    #exclude = ('an', )
+    list_filter = ('an', )
+
+class ClientInline(admin.TabularInline):
+    model = Client
+
+
+class RepAdmin(admin.ModelAdmin):
+    inlines = [ClientInline]
+
+
 admin.site.register(Student)
-admin.site.register(Courses)
+admin.site.register(Courses, CourseAdmin)
 admin.site.register(Client)
-admin.site.register(Reprezentant)
+admin.site.register(Reprezentant, RepAdmin)
