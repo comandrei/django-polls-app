a1e11f18d2f8e5e35c38cf7426a757d2175bca41
Andrei Coman <comandrei@gmail.com>
2020-06-09 08:28:51 +0300

    Add Question endpoint

--- src/polls/polls/urls.py
+++ src/polls/polls/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('questionnaire/', include('questionnaire.urls')),
+    path('api/', include('rest_api.urls'))
 ]
--- src/polls/rest_api/views.py
+++ src/polls/rest_api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import QuestionSerializer
+from question.models import Question
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
